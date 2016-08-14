b96e5159dd0bf54e7df18d7b94dfc8f850f4157a
vasilisL <vasilis.liras@gmail.com>
2016-08-14 23:00:16 +0300

    Update bdaylist8dig.py

--- bdaylist8dig.py
+++ bdaylist8dig.py
@@ -4,8 +4,8 @@ string=''
 FILE = open("bdl8dig.txt","w")
 item_count = 0
 
-for d in range(01, 32):
-    for m in range(01, 13):
+for d in range(1, 32):
+    for m in range(1, 13):
         for y in range(1939 , 2020):
             string = string + str(d).zfill(2) + str(m).zfill(2) + str(y)
             item_count += 1
