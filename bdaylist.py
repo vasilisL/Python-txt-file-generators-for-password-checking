ed7fffb652c4c30ea14d9442373c22d223b619e2
vasilisL <vasilis.liras@gmail.com>
2016-08-14 23:00:37 +0300

    Update bdaylist.py

--- bdaylist.py
+++ bdaylist.py
@@ -4,8 +4,8 @@ string=''
 FILE = open("bdl.txt","w")
 item_count = 0
 
-for d in range(01, 32):
-    for m in range(01, 13):
+for d in range(1, 32):
+    for m in range(1, 13):
         for y in range(1939 , 2020):
             string = string + str(d) + str(m) + str(y)
             item_count += 1
